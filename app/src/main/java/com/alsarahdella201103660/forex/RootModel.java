5d9cd82d4736c0bb2349de92a6aaa3b056424045
alsrhdella10 <132431129+alsrhdella10@users.noreply.github.com>
2023-06-21 14:04:05 +0700

    dela

---
+++ app/src/main/java/com/alsarahdella201103660/forex/RootModel.java
@@ -0,0 +1,18 @@
+package com.alsarahdella201103660.forex;
+
+import com.google.gson.annotations.SerializedName;
+
+import kotlin.jvm.internal.SerializedIr;
+
+public class RootModel
+{
+    @SerializedName("rates")
+    private RatesModel ratesModel;
+
+    public RootModel() {}
+
+    public RatesModel getRatesModel() { return ratesModel; }
+
+    public void setRatesModel(RatesModel ratesModel) {this.ratesModel = ratesModel; }
+}
+
